8a0e37cfd74054abbcc7dbce381d2dfc47719624
Berkay İNANLI <58389380+RhykerrR@users.noreply.github.com>
2021-08-24 15:52:26 +0300

    Update whois_status.py
    Updated according to the changes in whois_auto

--- whois_status.py
+++ whois_status.py
@@ -1,14 +1,13 @@
 import re
 
-
 status_list = []
 
 with open("domain_out.txt", "r") as f:
     content = f.read()   
-pattern = "([a-z0-9].+[a-z0-9]+.\s\s)(?:[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}.|[0-9]{4}-[a-zA-Z0-9]{2,3}-[0-9]{2}) (?:(?:(?:[A-z0-9]+(?:-[A-z0-9]+)*\.)+[A-z]{2,})) ([a-zA-Z]+\s[a-zA-Z]+://(?:[a-zA-Z]+(?:\.[a-zA-Z]+)+)/[a-zA-Z]+#[a-zA-Z]+)"
+pattern = "^([a-zA-Z]+(?:\.[a-zA-Z]+)+).*$\n.+\n.+.\n.(.+)"
 check = re.findall(pattern, content, re.MULTILINE)
 
 for i in check:
-    (domain, status) = i    
+    (domain, status) = i  
     if not status.startswith("ok"):
         print (domain.rstrip() + " --> " + status)
