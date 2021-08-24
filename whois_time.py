bd9610c3690b7ec2685afb2733686abb1921c9b5
Berkay İNANLI <58389380+RhykerrR@users.noreply.github.com>
2021-08-24 15:51:44 +0300

    Update whois_time.py
    Updated according to the changes on whois_auto

--- whois_time.py
+++ whois_time.py
@@ -6,12 +6,11 @@ sender = input ("Enter sender email: ")
 password = input("Enter password: ")
 receiver = input("Enter receiver email: ")
 
-
-
 with open("domain_out.txt", "r") as f:
     content = f.read()
 pattern = "\d{4}[/-]\w{2,3}[/-]\d{2}"
 dates = re.findall(pattern, content)
+
 
 fmt = '%d/%B/%Y'
 list = []
@@ -32,7 +31,7 @@ d1 = list
 d2 = datetime.today().strftime(fmt)
 d2 = datetime.strptime(d2, fmt)
 
-domains = re.findall(".*\s\s(?=\d{4}[/-]\w{2,3}[/-]\d{2})", content)
+domains = re.findall("([a-zA-Z]+(?:\.[a-zA-Z]+)+)\n.(?=\d{4}[/-]\w{2,3}[/-]\d{2})", content)
 domains = map(lambda s: s.strip(), domains)
 main_list = [' '.join(x) for x in zip(domains,d1)] 
 
@@ -48,15 +47,13 @@ for i in main_list:
             None
 
 
-    if 0 <= d <= 60: #gives warning in 60 days
+    if 0 <= d <= 60: #give warning in 60 days
         
         message_list.append("Domain " + i.pop(0) + " is getting timed out in " + i.pop() + ". Only " + str(d) + " days left.") 
         
 
-
 message_list.append("These are all the domains that are getting timed out in 60 days!")
 message = ' \n\n'.join([str(item) for item in message_list])
-
 
 server = smtplib.SMTP('smtp.gmail.com', 587)
 server.ehlo()
