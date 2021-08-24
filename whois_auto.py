910a63c51208d04f8d782e712565d96fb931ad7f
Berkay İNANLI <58389380+RhykerrR@users.noreply.github.com>
2021-08-24 15:47:14 +0300

    Updated name server query
    Now all the name servers are printed

--- whois_auto.py
+++ whois_auto.py
@@ -1,6 +1,5 @@
 import subprocess
 import re
-import data
 import tldextract
 from data_if_loop import domain_lookup
 
@@ -8,7 +7,7 @@ with open("domain_list.txt") as f:
     domain_all = f.read().splitlines()
 
 with open ("domain_out.txt", "w") as f:
-    f.write ("")
+    f.write("")
 
 temp = []
 
@@ -28,29 +27,33 @@ for domain in temp:
     d = tldextract.extract(domain)
     d = "{}".format(d.suffix)
     temp = set(d)
-     
-   
+  
     tld = domain_lookup(domain)
-    
-    
+     
     def whois(ip,name):
         p = subprocess.Popen(['whois', ip], stdout=subprocess.PIPE)
         out, err = p.communicate()
         m = re.search(tld.get(name), out.decode('ISO-8859-9'))
         return m.group(1)
     
-    
+    def whois_server(ip,name):
+        p = subprocess.Popen(['whois', ip], stdout=subprocess.PIPE)
+        out, err = p.communicate()
+        try:
+            m = re.findall(tld.get(name), out.decode('ISO-8859-9'))
+        except:
+            m = re.findall(tld.get(name), out.decode('ISO-8859-9')).group(1)
+        return m
     
     with open("domain_out.txt", "a") as f: 
         f.writelines(domain)
         f.writelines("\n")
         try:
-            f.writelines(" " + whois(domain, "expiration_date"))
-            f.writelines(" " + whois(domain, "name_servers"))
-            f.writelines(" " + whois(domain, "status"))
+            f.writelines(" " + whois(domain, "expiration_date") + "\n")
+            f.writelines(" " + str(whois_server(domain, "name_servers")) + "\n")
+            f.writelines(" " + whois(domain, "status") + "\n")
         except:
-            f.writelines("No info found!")
+            f.writelines("\n")
         f.writelines("\n")
   
- 
-
+  
