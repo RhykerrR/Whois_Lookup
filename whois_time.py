db8581ff7c7bce273fa34d3b772946a8e68866d4
Berkay İNANLI <58389380+RhykerrR@users.noreply.github.com>
2021-08-25 11:46:28 +0300

    Added more support for different time formats

--- whois_time.py
+++ whois_time.py
@@ -8,30 +8,74 @@ receiver = input("Enter receiver email: ")
 
 with open("domain_out.txt", "r") as f:
     content = f.read()
-pattern = "\d{4}[/-]\w{2,3}[/-]\d{2}"
+pattern = "\d{4}[/.-]\w{2,3}[/.-]\d{2}|\d{8}|\d{1,2}[/.-]\w{1,3}[/.-]\d{4}|\w{1,4}[ ]\w{2,3}[ ]\d{4}"
 dates = re.findall(pattern, content)
 
 
 fmt = '%d/%B/%Y'
-list = []
+date_list = []
 message_list = []
 
 for date in dates:        
-    if "-" in date:               
+    
+    if "-" in date:   
         try:          
             date = datetime.strptime(date, '%Y-%m-%d').strftime(fmt)
                    
-        except ValueError:                      
+        except ValueError:                         
+            try:
+                date = datetime.strptime(date, '%Y-%b-%d').strftime(fmt)
+        
+            except ValueError:
+                try:
+                    date = datetime.strptime(date, '%d-%b-%Y').strftime(fmt)           
+        
+                except ValueError:
+                    date = datetime.strptime(date, '%d-%m-%Y').strftime(fmt)
+                    
+                    
+    elif "." in date:                     
+        try:
+            date = datetime.strptime(date, '%Y.%m.%d').strftime(fmt)
             
-            date = datetime.strptime(date, '%Y-%b-%d').strftime(fmt)
+        except ValueError:
+            date = datetime.strptime(date, '%d.%m.%Y').strftime(fmt)
             
-    list.append(date)
-          
-d1 = list
+    elif " " in date:
+        try:
+            date = datetime.strptime(date, '%b %d %Y').strftime(fmt)
+            
+        except ValueError:
+            try:
+                date = datetime.strptime(date, '%dst %b %Y').strftime(fmt)
+            except ValueError:
+                try:
+                    date = datetime.strptime(date, '%dnd %b %Y').strftime(fmt)
+                except ValueError:
+                    try:
+                        date = datetime.strptime(date, '%drd %b %Y').strftime(fmt)
+                    except ValueError:
+                        try:
+                            date = datetime.strptime(date, '%dth %b %Y').strftime(fmt)
+                        except ValueError:
+                            pass
+    elif "/" in date:  
+        try:
+            date = datetime.strptime(date, '%d/%m/%Y').strftime(fmt)
+    
+        except ValueError:
+            pass
+    else:
+        date = datetime.strptime(date, '%Y%m%d').strftime(fmt)
+            
+    date_list.append(date)
+
+         
+d1 = date_list
 d2 = datetime.today().strftime(fmt)
 d2 = datetime.strptime(d2, fmt)
 
-domains = re.findall("([a-zA-Z]+(?:\.[a-zA-Z]+)+)\n.(?=\d{4}[/-]\w{2,3}[/-]\d{2})", content)
+domains = re.findall("([a-zA-Z]+(?:\.[a-zA-Z]+)+)\n.+(?=\d{4}[/.-]\w{2,3}[/.-]\d{2}|\d{8}|\d{1,2}[/.-]\w{1,3}[/.-]\d{4}|\w{1,4}[ ]\w{2,3}[ ]\d{4})", content)
 domains = map(lambda s: s.strip(), domains)
 main_list = [' '.join(x) for x in zip(domains,d1)] 
 
@@ -65,7 +109,5 @@ server.sendmail(sender, receiver, message)
 server.quit()
 
 print ("Email sent!")
-
-
 
 
